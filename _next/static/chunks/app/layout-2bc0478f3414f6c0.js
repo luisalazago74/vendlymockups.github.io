aa0fb5a009335fa12a9ba907877f1cfce9e62d05
Luis <luis.salazar@vendly.co>
2024-12-20 15:21:28 -0500

    Updates

---
+++ _next/static/chunks/app/layout-2bc0478f3414f6c0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1427:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,9840,23)),Promise.resolve().then(l.t.bind(l,9324,23))},9324:()=>{},9840:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_1f2b32"}}},e=>{var s=s=>e(e.s=s);e.O(0,[385,441,517,358],()=>s(1427)),_N_E=e.O()}]);
